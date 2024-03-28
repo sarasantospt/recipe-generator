6a37177344a551eec5af693d8874720e591616cd
sarasantospt <sarasantos95pt@gmail.com>
2024-03-28 21:03:15 +0000

    Added basic form handling and also Typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateRecipe(event) {
+  event.preventDefault();
 
+  new Typewriter("#recipe", {
+    strings: "1 cup of brown sugar",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let recipeFormElement = document.querySelector("#recipe-generator-form");
+recipeFormElement.addEventListener("submit", generateRecipe);
