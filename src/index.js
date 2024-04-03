3ef88aee40643d394bd0308d30984b59baa42e6e
sarasantospt <sarasantos95pt@gmail.com>
2024-04-03 22:52:26 +0100

    Small change in context

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ function generateRecipe(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "aa9affb5f453t7413c8f0o23076a944a";
   let context =
-    "You are very passionate master of cooking and love amazing and simple recipes. Your mission is to write delicious, easy to cook recipes. The recipes needs to be provided in HTML Avoid adding ```html at the beginning of the answer. Please make sure you follow the user instructions. Include a very small title with the dish name that is the same size as the recipe font. Sign the poem with 'Enjoy this delicious food! Happy Cooking/Baking!' inside a <strong> element at the end of the recipe.";
+    "You are very passionate master of cooking and love amazing and simple recipes. Your mission is to write delicious, easy to cook recipes. The recipes need to be provided in HTML. Avoid adding ```html at the beginning of the answer. Please make sure you follow the user instructions. Include a very small title with the dish name that is the same size as the recipe font. Sign the poem with 'Enjoy this delicious food! Happy Cooking/Baking!' inside a <strong> element at the end of the recipe.";
   let prompt = `User instructions: Generate a delicious recipe about ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
